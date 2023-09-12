2b35c9c5f3a1341a73cd347ba66693dee3d9932f
Ulises Cappa <ulisescappa@gmail.com>
2023-09-12 13:07:48 -0300

    Config browserRouter. Create NavBar, Details and Home components. Define some basic routes to display and test the components

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li> <Link to="/details">para Details</Link> </li>
+                <li><a href="">Most views</a></li>
+                <li><a href="">micIcon</a></li>
+                <li><a href="">ConfIcon</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
